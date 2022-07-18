c756fe1cd867280812df27728ea765a2dc1b6018
Emily Mitcheson-Smith <106613906+emmymitch@users.noreply.github.com>
2022-07-18 16:07:47 +0100

    Added title prop; adjusted BEM

--- src/components/EmployeeCard/EmployeeCard.jsx
+++ src/components/EmployeeCard/EmployeeCard.jsx
@@ -1,13 +1,13 @@
 import "./EmployeeCard.scss";
 import Counter from "../Counter/Counter";
 
-export const EmployeeCards = ({team}) => {
+export const EmployeeCards = ({team, title}) => {
     const cardList = team.map((employee) => {
         return (
             <div key={employee.id} className="team-card">
-                <h3>{employee.name}</h3>
-                <p>{employee.role}</p>
-                <Counter />
+                <h3 className="team-card__name">{employee.name}</h3>
+                <p className="team-card__role">{employee.role}</p>
+                <Counter title={title} />
             </div>
         )
     })
